317661940a6c2eec43112edef3a4c8af902ea651
Myeongseong Kim <morph@myeongseong.kim>
2025-07-10 13:52:11 +0900

    feat: custom poses event

--- telekinesis-scene/js/manipulator/manipulator.js
+++ telekinesis-scene/js/manipulator/manipulator.js
@@ -1,3 +1,6 @@
+import { PinchPose } from '../poses/pinch-pose.js';
+import { GrabPose } from '../poses/grab-pose.js';
+import { LockPose } from '../poses/lock-pose.js';
 import { ModeManager } from '../mode-manager.js';
 import { IdleMode } from './manipulator-modes/idle-mode.js';
 import { UniManualMode } from './manipulator-modes/uni-manual-mode.js';
@@ -19,46 +22,85 @@ AFRAME.registerComponent('manipulator', {
     this.leftHandEntity = this.el.sceneEl.querySelector('[hand-tracking-controls="hand: left"]');
     this.rightHandEntity = this.el.sceneEl.querySelector('[hand-tracking-controls="hand: right"]');
 
+    this.leftHandPose = this.leftHandEntity.components['hand-pose-controls'];
+    this.rightHandPose = this.rightHandEntity.components['hand-pose-controls'];
+
     this.targetEntity = this.data.target;
     this.planeEntity = this.data.plane;
-
-    this.pinchStartHandler = this.onPinchStart.bind(this);
-    this.pinchEndHandler = this.onPinchEnd.bind(this);
-
-    if (this.leftHandEntity) {
-      this.leftHandEntity.addEventListener('pinchstarted', this.pinchStartHandler);
-      this.leftHandEntity.addEventListener('pinchended', this.pinchEndHandler);
-    }
-    if (this.rightHandEntity) {
-      this.rightHandEntity.addEventListener('pinchstarted', this.pinchStartHandler);
-      this.rightHandEntity.addEventListener('pinchended', this.pinchEndHandler);
-    }
-  },
-
-  onPinchStart: function (evt) {
-    this.modeManager.currentMode.handlePinchStart(evt.target);
-  },
-
-  onPinchEnd: function (evt) {
-    this.modeManager.currentMode.handlePinchEnd(evt.target);
   },
 
   tick: function (time, deltaTime) {
+    if (this.leftHandPose) {
+      let leftCurrentPose = this.leftHandPose.currentPose;
+      let leftPreviousPose = this.leftHandPose.previousPose;
+      if (!leftCurrentPose || !leftPreviousPose) return;
+
+      if (PinchPose.isSelected(leftCurrentPose) && !PinchPose.isSelected(leftPreviousPose)) {
+        // console.log('Left hand pinch started');
+        this.modeManager.currentMode.handlePinchStart(this.leftHandEntity);
+      }
+      else if (!PinchPose.isSelected(leftCurrentPose) && PinchPose.isSelected(leftPreviousPose)) {
+        // console.log('Left hand pinch ended');
+        this.modeManager.currentMode.handlePinchEnd(this.leftHandEntity);
+      }
+
+      if (GrabPose.isSelected(leftCurrentPose) && !GrabPose.isSelected(leftPreviousPose)) {
+        // console.log('Left hand grab started');
+        this.modeManager.currentMode.handleGrabStart(this.leftHandEntity);
+      }
+      else if (!GrabPose.isSelected(leftCurrentPose) && GrabPose.isSelected(leftPreviousPose)) {
+        // console.log('Left hand grab ended');
+        this.modeManager.currentMode.handleGrabEnd(this.leftHandEntity);
+      }
+
+      if (LockPose.isSelected(leftCurrentPose) && !LockPose.isSelected(leftPreviousPose)) {
+        // console.log('Left hand lock started');
+        this.modeManager.currentMode.handleLockStart(this.leftHandEntity);
+      }
+      else if (!LockPose.isSelected(leftCurrentPose) && LockPose.isSelected(leftPreviousPose)) {
+        // console.log('Left hand lock ended');
+        this.modeManager.currentMode.handleLockEnd(this.leftHandEntity);
+      }
+    }
+    if (this.rightHandPose) {
+      let rightCurrentPose = this.rightHandPose.currentPose;
+      let rightPreviousPose = this.rightHandPose.previousPose;
+      if (!rightCurrentPose || !rightPreviousPose) return;
+
+      if (PinchPose.isSelected(rightCurrentPose) && !PinchPose.isSelected(rightPreviousPose)) {
+        // console.log('Right hand pinch started');
+        this.modeManager.currentMode.handlePinchStart(this.rightHandEntity);
+      }
+      else if (!PinchPose.isSelected(rightCurrentPose) && PinchPose.isSelected(rightPreviousPose)) {
+        // console.log('Right hand pinch ended');
+        this.modeManager.currentMode.handlePinchEnd(this.rightHandEntity);
+      }
+
+      if (GrabPose.isSelected(rightCurrentPose) && !GrabPose.isSelected(rightPreviousPose)) {
+        // console.log('Right hand grab started');
+        this.modeManager.currentMode.handleGrabStart(this.rightHandEntity);
+      }
+      else if (!GrabPose.isSelected(rightCurrentPose) && GrabPose.isSelected(rightPreviousPose)) {
+        // console.log('Right hand grab ended');
+        this.modeManager.currentMode.handleGrabEnd(this.rightHandEntity);
+      }
+
+      if (LockPose.isSelected(rightCurrentPose) && !LockPose.isSelected(rightPreviousPose)) {
+        // console.log('Right hand lock started');
+        this.modeManager.currentMode.handleLockStart(this.rightHandEntity);
+      }
+      else if (!LockPose.isSelected(rightCurrentPose) && LockPose.isSelected(rightPreviousPose)) {
+        // console.log('Right hand lock ended');
+        this.modeManager.currentMode.handleLockEnd(this.rightHandEntity);
+      }
+    }
+
     if (this.modeManager.currentMode) {
       this.modeManager.currentMode.execute();
     }
   },
 
-  remove: function () {
-    if (this.leftHandEntity) {
-      this.leftHandEntity.removeEventListener('pinchstarted', this.pinchStartHandler);
-      this.leftHandEntity.removeEventListener('pinchended', this.pinchEndHandler);
-    }
-    if (this.rightHandEntity) {
-      this.rightHandEntity.removeEventListener('pinchstarted', this.pinchStartHandler);
-      this.rightHandEntity.removeEventListener('pinchended', this.pinchEndHandler);
-    }
-  },
+  remove: function () { },
 });
 
 export function setWorldTransform(entity, pos, rot, scl) {
